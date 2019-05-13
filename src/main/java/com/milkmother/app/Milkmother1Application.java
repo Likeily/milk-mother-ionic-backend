8b8d991ccf68048c09382748588d8b4fff62841b
Likeily <likeilyms@gmail.com>
2019-05-13 13:19:12 -0300

    Criação de duplicação do projeto com algumas modificações para testes

---
+++ src/main/java/com/milkmother/app/Milkmother1Application.java
@@ -0,0 +1,13 @@
+package com.milkmother.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Milkmother1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Milkmother1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/milkmother/app/Milkmother1ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.milkmother.app;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Milkmother1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
