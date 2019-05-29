fad4029d6472dbda0f5268ebbc2d82ab61f8db93
Likeily <likeilyms@gmail.com>
2019-05-29 18:12:41 -0300

    Listando todas as categorias, deletando e alterando as categorias

--- src/main/java/com/milkmother/app/resources/CategoriaResource.java
+++ src/main/java/com/milkmother/app/resources/CategoriaResource.java
@@ -1,25 +1,62 @@
 package com.milkmother.app.resources;
 
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.milkmother.app.domain.Categoria;
+import com.milkmother.app.dto.CategoriaDTO;
 import com.milkmother.app.services.CategoriaService;
 
 @RestController
-@RequestMapping(value="/categorias")
+@RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
 	@Autowired
 	private CategoriaService service;
-	
-	@RequestMapping(value="/{id}", method=RequestMethod.GET)
-	public ResponseEntity<?> find(@PathVariable Integer id) {
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<Categoria> find(@PathVariable Integer id) {
 		Categoria obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
+	}
+
+	// Insere objeto no BD e usa o Id como argumento da uri
+	@RequestMapping(method = RequestMethod.POST)
+	// @RequestBody faz o Json convertido a onjeto java automaticamente
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+
+	// Atualiza
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody Categoria obj, @PathVariable Integer id) {
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 }
--- src/main/java/com/milkmother/app/services/CategoriaService.java
+++ src/main/java/com/milkmother/app/services/CategoriaService.java
@@ -1,12 +1,15 @@
 package com.milkmother.app.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.milkmother.app.domain.Categoria;
 import com.milkmother.app.repositories.CategoriaRepository;
+import com.milkmother.app.services.exceptions.DataIntegrityException;
 import com.milkmother.app.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -23,4 +26,30 @@ public class CategoriaService {
 		}
 		return obj.orElse(null);
 	}
+	
+	//o objeto novo a ser inserido tem que ter o id nulo, caso contrário será atualização.
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return repositori.save(obj);
+	}
+	
+	public Categoria update(Categoria obj) {
+		find(obj.getId());
+		return repositori.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+		repositori.deleteById(id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possivél excluir uma categoria que possui Beneficiários");
+		}
+	}
+	
+	public List<Categoria> findAll(){
+		return repositori.findAll();
+		}
+	
 }
